7cb6174cab3fa246a3bffc00684c549b864d698d
Kevin <takearest118@gmail.com>
2012-11-28 09:45:48 +0900

    Get png file from python challenge site

--- level7.py
+++ level7.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import Image
+import urllib2
+import StringIO
 import re
 
 def answer1():
-	im = Image.open("./oxygen.png")
+	url = "http://www.pythonchallenge.com/pc/def/oxygen.html"
+	print url
+	image_url = "http://www.pythonchallenge.com/pc/def/oxygen.png"
+	response = urllib2.urlopen(image_url)
+	raw = response.read()
+	im = Image.open(StringIO.StringIO(raw))
 	pixels = [im.getpixel((x, im.size[1]/2)) for x in range(0, im.size[0], 7)]
 	print pixels
 	same_rgb = [r for r,g,b,a in pixels if r==g==b]
